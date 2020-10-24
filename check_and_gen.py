f462ffa33058f786b52fc83099e8a2f051e628d7
mxrch <kvwaljam@protonmail.ch>
2020-10-24 15:54:30 +0200

    improved check_and_gen.py

--- check_and_gen.py
+++ check_and_gen.py
@@ -1,15 +1,18 @@
 import json
 from os.path import isfile
+from pathlib import Path
 from ssl import SSLError
 
 import httpx
 from seleniumwire import webdriver
+from selenium.common.exceptions import TimeoutException as SE_TimeoutExepction
 
 import config
 from lib.utils import *
 
-from selenium.common.exceptions import TimeoutException as SE_TimeoutExepction
 
+# We change the current working directory to allow using GHunt from anywhere
+os.chdir(Path(__file__).parents[0])
 
 def get_saved_cookies():
     ''' returns cookie cache if exists '''
@@ -99,42 +102,48 @@ if __name__ == '__main__':
 
     cookies = {"__Secure-3PSID": "", "APISID": "", "SAPISID": "", "HSID": "", "CONSENT": "YES+FR.fr+V10+BX"}
 
-    ask = True
     new_cookies_entered = False
 
     if not cookies_from_file:
-        ask = False
         print("\nEnter these browser cookies found at accounts.google.com :")
         for name in cookies.keys():
             if not cookies[name]:
-                cookies[name] = input(f"{name} => ").strip()
+                cookies[name] = input(f"{name} => ").strip().strip('\"')
     else:
         # in case user wants to enter new cookies (for example: for new account)
+        html = get_authorization_source(cookies_from_file)
+        valid = False
+        if html:
+            print("\n[+] The cookies seems valid !")
+            valid = True
+        else:
+            print("\n[-] Seems like the cookies are invalid.")
         new_gen_inp = input("\nDo you want to enter new browser cookies from accounts.google.com ? (Y/n) ").lower()
         if new_gen_inp == "y":
             new_cookies_entered = True
             for name in cookies.keys():
                 if not cookies[name]:
-                    cookies[name] = input(f"{name} => ").strip()
+                    cookies[name] = input(f"{name} => ").strip().strip('\"')
+        elif not valid:
+            exit("Please put valid cookies. Exiting...")
 
 
     # validate cookies
-    if not new_cookies_entered:
-        cookies = cookies_from_file
-    html = get_authorization_source(cookies)
-    if html:
-        print("\n[+] The cookies seems valid! Generating the Google Docs and "
-            "Hangouts token...\n")
-    else:
-        exit("\n[-] Seems like the cookies are invalid, try regenerating them")
+    if new_cookies_entered or not cookies_from_file:
+        html = get_authorization_source(cookies)
+        if html:
+            print("\n[+] The cookies seems valid !")
+        else:
+            exit("\n[-] Seems like the cookies are invalid, try regenerating them.")
     
     if not new_cookies_entered:
-        if ask:
-            choice = input("Do you want to generate new Google Docs "
-                            "and Hangouts tokens ? (Y/n) ").lower()
-            if choice != "y":
-                exit()
+        cookies = cookies_from_file
+        choice = input("Do you want to generate new Google Docs "
+                        "and Hangouts tokens ? (Y/n) ").lower()
+        if choice != "y":
+            exit()
 
+    print("Generating the Google Docs and Hangouts token...\n")
     # get Google Docs token
     trigger = '\"token\":\"'
     if trigger not in html:
