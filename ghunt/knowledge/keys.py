32b0387afabeb0711cae74673e00b12d7adee938
Alon <125146479+MeHowHere@users.noreply.github.com>
2024-03-24 19:23:54 +0200

    Update keys.py
    new Key Update

--- ghunt/knowledge/keys.py
+++ ghunt/knowledge/keys.py
@@ -4,5 +4,5 @@ keys = {
     "apis_explorer": {"key": "AIzaSyAa8yy0GdcGPHdtD083HiGGx_S0vMPScDM", "origin": "https://explorer.apis.google.com"},
     "calendar": {"key": "AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs", "origin": "https://calendar.google.com"},
     "youtubei": {"key": "AIzaSyA8eiZmM1FaDVjRy-df2KTyQ_vz_yYM39w", "origin": "https://youtube.com"},
-    "drive": {"key": "AIzaSyD_InbmSFufIEps5UAt2NmB_3LvBH3Sz_8", "origin": "https://drive.google.com"}
+    "drive": {"key": "AIzaSyA65lEHUEizIsNtlbNo-l2K18dT680nsaM", "origin": "https://drive.google.com"}
 }
